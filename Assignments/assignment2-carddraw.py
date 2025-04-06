6d969459a6fd086225bec05f70b00c412bdfb7ca
Emmet Devaney <g00438866@atu.ie>
2025-04-06 19:01:37 +0100

    Assign - 2 - display cards

--- Assignments/assignment2-carddraw.py
+++ Assignments/assignment2-carddraw.py
@@ -12,3 +12,16 @@ data = response.json()
 deck_id = data['deck_id']
 
 print("Deck ID:", deck_id)
+
+
+
+#drawing 5 cards of the deck_id
+draw_url = f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=5"
+response = requests.get(draw_url)
+draw_data = response.json()
+cards = draw_data["cards"]
+
+#include value and suit
+print("Your cards:")
+for card in cards:
+    print(f"{card['value']} of {card['suit']}")
