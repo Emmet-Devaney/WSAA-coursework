1b5cf200780391c571e3eeaaa51ff4f18ebe8e02
Emmet Devaney <g00438866@atu.ie>
2025-04-18 19:01:33 +0100

    Assign - 4 - gitHubResponse

--- Assignments/assignment04-github.py
+++ Assignments/assignment04-github.py
@@ -10,19 +10,17 @@ repo = g.get_repo("Emmet-Devaney/Private-Repo")
 
 fileInfo = repo.get_contents("Wiki-Andrew.txt")
 
-
 urlOfFile = fileInfo.download_url
 
 response = requests.get(urlOfFile)
 contentOfFile = response.text
 
-#replace_content = contentOfFile.replace('Andrew', 'Emmet')
-replace_content = contentOfFile.replace('Emmet', 'Andrew')
+replace_content = contentOfFile.replace('Andrew', 'Emmet')
+#replace_content = contentOfFile.replace('Emmet', 'Andrew')
 
 
-print(replace_content[:200], "...")
+print(replace_content)
 
-'''
-gitHubResponse=repo.update_file(fileInfo.path,"updated by prog",newContents,fileInfo.sha)
-print (gitHubResponse)
-'''
+gitHubResponse = repo.update_file(fileInfo.path,"updated by program assignment04-github.py", replace_content, fileInfo.sha)
+
+print(gitHubResponse)
