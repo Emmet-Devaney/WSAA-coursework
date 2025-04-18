3d2ae5c9e5796fb1b45657360c4fbbd21214088b
Emmet Devaney <g00438866@atu.ie>
2025-04-18 18:53:36 +0100

    Assign - 4 - Andrew-Emmet text

--- Assignments/assignment04-github.py
+++ Assignments/assignment04-github.py
@@ -1,6 +1,6 @@
 from github import Github
 import requests
-from config import config as cfg
+from config import apikey as cfg
 
 apikey = cfg["assign4tk"]
 
@@ -10,17 +10,19 @@ repo = g.get_repo("Emmet-Devaney/Private-Repo")
 
 fileInfo = repo.get_contents("Wiki-Andrew.txt")
 
+
 urlOfFile = fileInfo.download_url
 
 response = requests.get(urlOfFile)
 contentOfFile = response.text
 
-replace_content = file_content.replace('Andrew', 'Emmet')
+#replace_content = contentOfFile.replace('Andrew', 'Emmet')
+replace_content = contentOfFile.replace('Emmet', 'Andrew')
 
+
+print(replace_content[:200], "...")
 
 '''
-newContents = contentOfFile + " more stuff 2 \n"
-print (newContents)
 gitHubResponse=repo.update_file(fileInfo.path,"updated by prog",newContents,fileInfo.sha)
 print (gitHubResponse)
 '''
